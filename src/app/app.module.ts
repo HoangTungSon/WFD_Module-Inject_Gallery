dd993964f0481fdb3f10ea0972130233b2c08cc8
hoang tung son <hoangtungson164@gmail.com>
2019-10-24 11:47:23 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+import { ImageCardComponent } from './image-gallery/image-card/image-card.component';
+import {ImageGalleryModule} from './image-gallery/image-gallery/image-gallery.module';
+import {GalleryConfig} from './image-gallery/token';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ImageGalleryComponent,
+    // ImageCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageGalleryModule
   ],
-  providers: [],
+  providers: [{
+    provide: GalleryConfig, useValue: 2
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
